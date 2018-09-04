87c1bbc1de63294a68148ff1399143eaa145a705
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-09-04 10:33:06 -0300

    Chamando QRcode

--- main.php
+++ main.php
@@ -4,9 +4,16 @@ require_once 'Pessoa.php';
 require_once 'Pessoa2.php';
 require_once 'vendor/autoload.php';
 
-use Projeto\Pessoa;
+// use Projeto\Pessoa;
 
-$pessoa = new Pessoa("Natan", 28, 1.73);
-$pessoa2 = new \Pessoa("Div", 20, 1.73);
-$pessoa->andar();
-$pessoa2->andar();
+// $pessoa = new Pessoa("Natan", 28, 1.73);
+// $pessoa2 = new \Pessoa("Div", 20, 1.73);
+// $pessoa->andar();
+// $pessoa2->andar();
+
+use Endroid\QrCode\QrCode;
+
+$qrCode = new QrCode('Not all who wander are lost');
+
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
